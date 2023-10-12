21603cd949757cb45e029106ed20b64e698db048
4ORTHER <Repfi.illy@gmail.com>
2023-10-12 17:54:47 +0700

    create linked list from csv

--- linked_list.py
+++ linked_list.py
@@ -1,17 +1,40 @@
+score_dict = {
+        1: 8,
+        2: 6,
+        3: 5,
+        4: 4,
+        5: 3,
+        6: 2,
+        7: 1,
+        8: 0
+}
+
 class Node:
-        def __init__(self, index, name, kills):
+        def __init__(self, index: int, name: str, kills: int):
                 self.index = index
                 self.name = name
                 self.kills = kills
-                self.score = 0
+                self.score = score_dict.get(index)
                 self.next_node = None
 
-class SinglyLinkedList:
+class LinkedList:
         def __init__(self):
                 self.head_node = None
 
-        def print_list(self):
+        def display(self):
                 node = self.head_node
-                while node is not None:
-                        print(f'{node.id}|{node.name}|{node.kills}|{node.score}')
+                while node:
+                        print('-'*200)
+                        print(f'{node.index}|{node.name}|{node.kills}|{node.score}')
                         node = node.next_node
+
+        # Method to insert data into the linked list
+        def insert(self, index: int, name: str, kills: int):
+                new_node = Node(index, name, kills)
+                if self.head_node is None:
+                        self.head_node = new_node
+                else:
+                        current = self.head_node
+                        while current.next_node:
+                                current = current.next_node
+                        current.next_node = new_node
--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 from text_extraction import TextExtraction
+from linked_list import LinkedList
+import os
+import pandas as pd
+
+def create_linked_list(folder: str, linked_list: LinkedList):
+        # Iterate through files in the folder
+        for filename in os.listdir(folder):
+            if filename.endswith('.csv'):  # Check if the file is a CSV file
+                file_path = os.path.join(folder, filename)
+                
+                # Read the CSV file into a pandas DataFrame without considering the first row as column names
+                df = pd.read_csv(file_path, header=None)
+                df.columns = df.loc[0]
+                df = df.drop(0)
+
+                # Iterate through rows in the DataFrame and insert data into the linked list
+                for _, row in df.iterrows():
+                    data = row.values
+
+                    # if type(int(data[0])) == int: 
+                    #     print(data)
+                    linked_list.insert(int(data[0]) + 1, data[1].strip(), int(data[2]))
 
 path_folder = r'images/DOW-mini-tour-30/Game1/'
-destination_folder = r'results/Game1/'
+destination_folder = r'extracted_text/Game1/'
 
 text_extraction = TextExtraction()
-text_extraction.extract_text(path_folder, destination_folder)
+
+# text_extraction.extract_text(path_folder, destination_folder)
+game1 = LinkedList()
+create_linked_list(destination_folder, game1)
+game1.display()
