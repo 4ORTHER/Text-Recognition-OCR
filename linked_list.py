199e6a14e7fb24ff86e635ff85185c64363a1e2d
4ORTHER <Repfi.illy@gmail.com>
2023-10-12 23:02:04 +0700

    add export_to_csv function

--- linked_list.py
+++ linked_list.py
@@ -25,22 +25,22 @@ class LinkedList:
 
         def display(self):
                 node = self.head_node
+                print('-'*200)
                 while node:
-                        print('-'*200)
                         print(f'{node.index}|{node.name}|{node.kills}|{node.score}')
                         node = node.next_node
+                print('-'*200)
 
         # Method to insert data into the linked list
         def insert(self, index: int, name: str, kills: int):
                 new_node = Node(index, name, kills)
-                if self.head_node is None:
-                        self.head_node = new_node
-                elif new_node.score >= self.head_node.score:
+
+                if self.head_node is None or new_node.score > self.head_node.score or (new_node.score == self.head_node.score and new_node.kills > self.head_node.kills):
                         new_node.next_node = self.head_node
                         self.head_node = new_node
                 else:
                         current = self.head_node
-                        while current.next_node and new_node.score < current.next_node.score:
+                        while current.next_node and (new_node.score < current.next_node.score or (new_node.score == current.next_node.score and new_node.kills < current.next_node.kills)):
                                 current = current.next_node
                         new_node.next_node = current.next_node
                         current.next_node = new_node
@@ -49,8 +49,17 @@ class LinkedList:
                 data = []
                 current = self.head_node
                 while current:
-                        data.append([current.name, current.kills, current.score])
+                        data.append([current.name, current.score, current.kills])
                         current = current.next_node
 
-                        df = pd.DataFrame(data, columns=['รายชื่อ', 'Kills', 'คะแนน'])
-                        df.to_csv('output/' + filename, index=True)
+                df = pd.DataFrame(data, columns=['Name', 'Score', 'Kills'])
+                df.to_csv('output/' + filename + '.csv', index=True)
+
+        def export_to_excel(self, filename: str):
+                data = []
+                current = self.head_node
+                while current:
+                        data.append({'Name': current.name, 'Score': current.score, 'Kills': current.kills})
+                        current = current.next_node
+                df = pd.DataFrame(data)
+                df.to_excel('output/' + filename + '.xlsx', index=True)
